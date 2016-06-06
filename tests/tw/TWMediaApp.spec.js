bef47121e0eb4e98a1b16dae864f7a08733d1a89
mahidhar <mahireddy.1989@gmail.com>
2016-06-06 02:40:14 +0100

    Commit message

--- tests/tw/TWMediaApp.spec.js
+++ tests/tw/TWMediaApp.spec.js
@@ -39,7 +39,7 @@ describe('travelweekly Media AppAdminPage', function () {
     beforeEach(beforeEachSetup);
 
     function beforeEachSetup() {
-        browser.get('http://staging.travelweekly.co.uk/app/admin/login');
+        browser.get('');
         browser.driver.manage().window().maximize();
 
     }
