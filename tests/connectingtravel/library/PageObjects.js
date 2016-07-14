2cc7018eb6b5d6d61a076c3f0130f7cc619bc18d
mahidhar <mahireddy.1989@gmail.com>
2016-07-14 14:36:13 +0100

    pae object model added

---
+++ tests/connectingtravel/library/PageObjects.js
@@ -0,0 +1,31 @@
+/*
+
+    var PageObjects = function () {
+
+
+    function pageobjects(){
+
+        this.Element = element(by.xpath('//*[@id="telugunews_menu"]/ul/li[2]/a'
+
+    }
+
+
+    /*
+
+     this. verifyListItems = function(listItemsExpected,listActual, offset){
+     var i;
+     var expectedItemsString;
+     for (i=0;i<listItemsExpected.length; i++)
+
+     expectedItemsString = expectedItemsString+ listItemsExpected[i];
+     this.waitForElement(null,browser.time(), listActual, true);
+
+     list
+     */
+
+
+    module.exports = PageObjects();
+
+
+
+
--- tests/yopauk/evalutionform.spec.js
+++ tests/yopauk/evalutionform.spec.js
@@ -1,5 +1,12 @@
 
+
+var width = 750;
+var height = 1334;
+
+
 fdescribe('YOPA todo list', function () {
+
+
 
 
     beforeEach(beforeEachSetup);
@@ -7,27 +14,39 @@ fdescribe('YOPA todo list', function () {
     function beforeEachSetup() {
 
         browser.ignoreSynchronization = true;
-        browser.get('http://qa.yopa.uk/');
+
+        browser.get('http://telugunews4u.com/');
+        browser.driver.manage().window().setSize(width, height);
+
     }
 
     describe('EvaltionForm', function () {
         it('should navigate to navigate to the work flow', function () {
 
 
+            var Memu = element(by.xpath('//*[@id="telugunews_top_panel"]/button')).click();
 
-            var Element = driver.find(By.xpath('//*[@id="home"]/section/section[1]/div/div/div/div/div[1]/div/div[3]/div/form[1]/input'));
+//browser.pause();
 
-                Element.sendKeys('E62DZ');
+            //var eee = element(by.id('telugunews_menu')).click();
 
-
-
-
-
+           var Element = element(by.xpath('//*[@id="telugunews_menu"]/ul/li[2]/a'));
 
 
+                Element.click();
+                //sendKeys('E62DZ');
+
+//              Memu.click();
+
+  //          var subMitButton = element(by.xpath('//*[@id="telugunews_menu"]/ul/li[3]/a')).click();
 
 
         })
     })
 
 });
+
+
+//protractor works on non angular applications as well
+
+
